56ec417b48b2873d9613da92eb9f9b3c85d96875
naxwh <munnamon5@gmail.com>
2022-01-01 11:03:46 +0530

    update

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
-void main() => runApp(MaterialApp(
-      home: QuoteList(),
-    ));
+void main() => runApp(MaterialApp(home: QuoteList()));
 
 class QuoteList extends StatefulWidget {
-  QuoteList({Key? key}) : super(key: key);
-
   @override
   _QuoteListState createState() => _QuoteListState();
 }
@@ -16,13 +13,13 @@ class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes = [
     Quote(
         author: 'Oscar Wilde',
-        text: 'Be yourself; everyone else is already taken.'),
+        text: 'Be yourself; everyone else is already taken'),
     Quote(
         author: 'Oscar Wilde',
-        text: 'I have nothing to declare except my genius.'),
+        text: 'I have nothing to declare except my genius'),
     Quote(
         author: 'Oscar Wilde',
-        text: 'The truth is rarely pure and never simple.'),
+        text: 'The truth is rarely pure and never simple')
   ];
 
   @override
@@ -35,7 +32,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }

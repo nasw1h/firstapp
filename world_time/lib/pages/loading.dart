f0f95539e9bc9d96ad05fde4e13f81608411b21e
naxwh <munnamon5@gmail.com>
2022-01-01 22:46:26 +0530

    Update loading.dart

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -7,11 +7,11 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   String time = 'loading';
 
   void setupWorldTime() async {
-    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
     print(instance.time);
     setState(() {

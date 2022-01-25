0e35ef600abd401008e28f76aac6ea217966c8ee
naxwh <munnamon5@gmail.com>
2022-01-25 14:56:13 +0530

    Update main.dart

--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
+import 'package:world_time/pages/choose_location.dart';
 
 void main() => runApp(MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Loading(),
-        '/home': (context) => Home(),
-        '/location': (context) => ChooseLocation(),
-      },
-    ));
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
+));
